0aa5bd3fb575a890f3529f6f1cb8c1429cd899c4
Khaqul Huda <khaqulhudaa@gmail.com>
2023-12-06 09:41:26 +0700

    rank/_id:add search

--- pages/rank/_id.vue
+++ pages/rank/_id.vue
@@ -15,39 +15,39 @@
           >go back</a
         >
         <h1 class="lg:text-xl font-semibold">{{ title }}</h1>
-        <!-- <div class="relative">
-              <label class="sr-only" for="search"> Search </label>
+        <div class="relative">
+          <label class="sr-only" for="search"> Search </label>
 
-              <input
-                class="h-10 w-full rounded-full border-none bg-white pe-10 ps-4 text-sm shadow-sm sm:w-56"
-                id="search"
-                type="search"
-                placeholder="Cari nama anda"
-                @change="handleSearch"
-                v-model="searchQuery"
+          <input
+            class="h-10 w-full rounded-full border-none bg-white pe-10 ps-4 text-sm shadow-sm sm:w-56"
+            id="search"
+            type="search"
+            placeholder="Cari nama anda"
+            v-model="searchQuery"
+          />
+
+          <button
+            @click="fetchUser"
+            type="button"
+            class="absolute end-1 top-1/2 -translate-y-1/2 rounded-full bg-gray-50 p-2 text-gray-600 transition hover:text-gray-700"
+          >
+            <span class="sr-only">Search</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-4 w-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               />
-
-              <button
-                type="button"
-                class="absolute end-1 top-1/2 -translate-y-1/2 rounded-full bg-gray-50 p-2 text-gray-600 transition hover:text-gray-700"
-              >
-                <span class="sr-only">Search</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-4 w-4"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </button>
-            </div> -->
+            </svg>
+          </button>
+        </div>
       </div>
     </header>
     <div class="overflow-x-auto mt-4 mx-8">
@@ -98,7 +98,10 @@ export default {
   },
   methods: {
     async fetchUser() {
-      const { data, error } = await this.$supabase.from('users').select('*')
+      const { data, error } = await this.$supabase
+        .from('users')
+        .select('*')
+        .ilike('name', `%${this.searchQuery}%`)
 
       if (error) {
         console.error('Error fetching user:', error)
@@ -117,24 +120,12 @@ export default {
     },
     sortUsersByScore() {
       const id = this.$route.params.id
-
-      // Menggunakan fungsi sort untuk mengurutkan pengguna berdasarkan score
       this.users.sort((a, b) => {
         const scoreA = a[`${id}_score`] || 0
         const scoreB = b[`${id}_score`] || 0
 
         return scoreB - scoreA
       })
-    },
-    handleSearch() {
-      if (this.searchQuery === '') {
-      }
-      this.filterUsers()
-    },
-    filterUsers() {
-      this.filteredUsers = this.users.filter((user) =>
-        user.name.toLowerCase().includes(this.searchQuery.toLowerCase())
-      )
     },
     hideAlert() {
       setTimeout(() => {
@@ -146,7 +137,6 @@ export default {
     this.hideAlert()
     await this.fetchUser()
     this.sortUsersByScore()
-    console.log(this.users)
   },
 }
 </script>
